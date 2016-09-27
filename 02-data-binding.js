1662300381dd83e425dc9fe7c7928c102952b512
Michael Coulter <msc2086@gmail.com>
2016-09-26 20:28:44 -0600

    deleted unnecessary files

--- 02-data-binding.js
+++
@@ -1,6 +0,0 @@
-var user_input = document.getElementById("name");
-var output = document.getElementById("output")
-
-document.addEventListener("keyup", function(){
-  output.innerHTML = "Hello, my name is " + user_input.value
-})
--- 07-app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("broken", []);
-
-app.controller('MainController', function($scope) {
-  $scope.view={};
-  $scope.view.number = 42;
-});
